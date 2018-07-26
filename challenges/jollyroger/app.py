f216067afd66d95ec6d9ac9a896f49f9b4eeda53
shareef12 <shareef12@twelvetacos.com>
2018-07-25 23:21:47 -0400

    WIP: Add auto-update terminal using socket.io
    This implementation uses socket.io to transmit data to the client.
    It currently has a few drawbacks:
    1. polling - Due to the asyncio nature of the server socketio handlers,
       all calls to emit() will get buffered until the handler returns. To
       get near real-time updates, I'm just polling the server for updates
       every 200ms.
    2. id(request) isn't unique if a user refreshes the page. Something
       will need to be modified to fix this.

--- challenges/jollyroger/app.py
+++ challenges/jollyroger/app.py
@@ -1,13 +1,18 @@
+from fcntl import fcntl, F_GETFL, F_SETFL
 import os
+import subprocess
 
-from flask import Flask, flash, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_socketio import SocketIO, disconnect, emit
 from werkzeug.utils import secure_filename
 
 import signtool
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = b"f6bd38bacb77fa4dc5f35c963109bbdf0f70454ca935123a"
+socketio = SocketIO(app)
 
+PROCESSES = {}
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index", methods=["GET", "POST"])
@@ -37,4 +42,44 @@ def index():
         print("Verify success! ({:s})".format(image.filename))
         return redirect(request.url)
 
+    print("GET", id(request), request.remote_addr)
+    proc = subprocess.Popen("echo 'test' && sleep 1 && echo 'test2' && sleep 4 && echo 'test3'", shell=True, stdout=subprocess.PIPE)
+    flags = fcntl(proc.stdout, F_GETFL)
+    fcntl(proc.stdout, F_SETFL, flags | os.O_NONBLOCK)
+    PROCESSES[id(request)] = proc
     return render_template("index.html")
+
+
+@socketio.on("connect")
+def on_connect():
+    print("connect", id(request), request.sid, request.remote_addr)
+
+
+@socketio.on("disconnect")
+def on_disconnect():
+    print("disconnect", id(request), request.sid, request.remote_addr)
+    proc = PROCESSES.get(id(request))
+    if proc is not None:
+        proc.kill()
+        del PROCESSES[id(request)]
+
+
+@socketio.on("update")
+def on_update():
+    print("update", id(request), request.sid, request.remote_addr)
+    proc = PROCESSES.get(id(request))
+    if proc is not None:
+        try:
+            data = os.read(proc.stdout.fileno(), 1024)
+            data = data.replace(b"\n", b"\r\n")
+            print(data)
+            if data:
+                emit("console", {"data": data.decode("utf-8")})
+            else:
+                disconnect()
+        except OSError:
+            pass    # no data
+
+
+if __name__ == "__main__":
+    socketio.run(app)
