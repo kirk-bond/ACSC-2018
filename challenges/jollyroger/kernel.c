4358d8cb63f6d932c41acd19794343816ffa5a8f
shareef12 <shareef12@twelvetacos.com>
2018-07-27 00:06:17 -0400

    Update kernel to print the flag if patched correctly

--- challenges/jollyroger/kernel.c
+++ challenges/jollyroger/kernel.c
@@ -1,7 +1,9 @@
 #include <elf.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
@@ -12,6 +14,9 @@
 #else
 #define debug(fmt, ...)
 #endif
+
+#define VERSION_STR "SecureSystems Kernel 3.10.0"
+//#define VERSION_STR "jollyroger"
 
 uint32_t cksum1 __attribute__((section (".text"))) = 0xdeadbeef;
 uint32_t cksum2 __attribute__((section (".text"))) = 0xcafebabe;
@@ -41,6 +46,8 @@ int validate_checksums(void)
         phdr = (Elf64_Phdr *)((intptr_t)phdr + ehdr->e_phentsize);
     }
 
+    usleep(500 * 1000);
+
     // crc the segment, skipping cksum1 and cksum2
     size_t len = (intptr_t)&cksum1 - phdr->p_vaddr;
     debug("crc32(0, %p, %lx)\n", (void *)phdr->p_vaddr, len);
@@ -48,6 +55,8 @@ int validate_checksums(void)
     len = (intptr_t)phdr->p_vaddr + phdr->p_filesz - (intptr_t)(&cksum2 + 1);
     debug("crc32(crc1, %p, %lx)\n", (void *)(&cksum2 + 1), len);
     crc1 = crc32(crc1, (void *)(&cksum2 + 1), len);
+
+    usleep(500 * 1000);
 
     // only verify cksum1 (text segment) since the .got.plt section
     // was modified during load.
@@ -64,12 +73,29 @@ int validate_checksums(void)
 
 int main(void)
 {
-    int ret = validate_checksums();
+    int ret = 0;
+    int fd = -1;
+    char buf[128] = {0};
+
+    puts("(kernel) Starting self-validation...");
+    ret = validate_checksums();
     if (ret == 0) {
-        printf("secure!\n");
+        puts("(kernel) Validation succeeded!");
     }
     else {
-        printf("not secure.\n");
+        puts("(kernel) Validation failed!");
+        puts("(kernel) Kernel panic - corrupted image");
+        _exit(EXIT_FAILURE);
     }
+
+    printf("(kernel) Kernel version: %s\n", VERSION_STR);
+    if (strcmp(VERSION_STR, "jollyroger") == 0) {
+        fd = open("/flag", O_RDONLY);
+        if (fd >= 0) {
+            read(fd, buf, sizeof(buf) - 1);
+            printf("(kernel) %s\n", buf);
+        }
+    }
+
     return 0;
 }
