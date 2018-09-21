45a832afcf6eebffc4e1492e730a715c3a21e5d7
Troy Ward <pyrodie18@gmail.com>
2018-09-20 22:50:33 -0400

    fixed cookie problem with index.php and added Dockerfile

--- challenges/prize/index.php
+++ challenges/prize/index.php
@@ -1,5 +1,34 @@
+<?php 
+    if (!isset($_COOKIE['count']))
+    {
+        $count = strval(1);
+        setcookie("count", $count);
+        $checksum = base64_encode($count.":".md5(strval($count)));
+        setcookie("checksum", $checksum);
+    }
+    else
+    {
+        $count = $_COOKIE['count'];
+        $checksum = $_COOKIE['checksum'];
+        $calculated_checksum = base64_encode($count.":".md5(strval($count)));
+        if ($checksum == $calculated_checksum)
+        {
+            $count = ++$_COOKIE['count'];
+            $checksum = base64_encode($count.":".md5(strval($count)));
+            setcookie("count", $count);
+            setcookie("checksum", $checksum);
+            $validcheck = 1;
+        }
+        else
+        {
+           $validcheck = 0;
+        }
+    }
+?>  
+
 <html> 
-    <head> 
+    <head>
+
         <title>We Love to Give Stuff Away</title> 
 		<style>
 			body {
@@ -15,33 +44,22 @@
                                 color:  red;
 			}
 		</style>
+
 	</head> 
   <body> 
   <h1>We Love to Give Stuff Away</h1>
   <p>We have got an amazing prize for our lucky 1,000,000 visitor</p>
 <?php 
-    if (!isset($_COOKIE['count']))
+    if ($count == 1)
     {
-        $count = strval(1);
-        setcookie("count", $count);
-        $checksum = base64_encode($count.":".md5(strval($count)));
-        setcookie("checksum", $checksum);
         ?>
            <p> Unfortunately you are only visitor <?php echo $count; ?> so make sure you visit again soon </p>
         <?php
     }
     else
     {
-        $count = $_COOKIE['count'];
-        $checksum = $_COOKIE['checksum'];
-        $calculated_checksum = base64_encode($count.":".md5(strval($count)));
-        if ($checksum == $calculated_checksum)
-        {
-            $count = ++$_COOKIE['count'];
-            $checksum = base64_encode($count.":".md5(strval($count)));
-            setcookie("count", $count);
-            setcookie("checksum", $checksum);
-
+       if ($validcheck == 1)
+       {
             if ($count == 1000000)
             {
                 ?>
